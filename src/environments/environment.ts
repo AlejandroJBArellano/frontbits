3fcbae488d7041ec6e93ad86c0b109397d09bf21
Alejandro Jesús Arellano <jesusarellano334@gmail.com>
2023-01-21 00:47:26 -0600

    chore(dependencies): installed supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,8 @@ export const environment = {
     appId: "1:337906367299:web:23daf54a673a77ee6d8df6",
     measurementId: "G-5FKKWQ2JPM",
   },
-  reCaptchaProvider: '6Lc1t9QjAAAAADyMWqk_qqRTEm_yLo3uj7xbAsHA'
+  reCaptchaProvider: "6Lc1t9QjAAAAADyMWqk_qqRTEm_yLo3uj7xbAsHA",
+  supabaseUrl: "https://bzgdvvuxwcfqzlsnmmik.supabase.co",
+  supabaseKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6Z2R2dnV4d2NmcXpsc25tbWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQyODMxNTQsImV4cCI6MTk4OTg1OTE1NH0.o12GuIXDUK99QdPbLr502UL7ofDZUF60n9iAFS5c3IY",
 };
